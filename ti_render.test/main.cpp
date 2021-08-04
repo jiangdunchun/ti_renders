5e6bf5e51a55bd8347b7e595e87b836d692f1066
Jiang Dunchun <jiangdunchun@outlook.com>
2021-08-04 22:39:16 +0800

    feat(ti_render.test): add test program

--- ti_render.test/main.cpp
+++ ti_render.test/main.cpp
@@ -1,2 +1,19 @@
+#include <common\logger_management.cpp>
+#include <render.h>
+#include "console_logger.hpp"
+
+using namespace ti_render;
+
 int main(int, char**) {
+	console_logger c_logger;
+	logger_management::attach_logger(&c_logger);
+	
+	render::init(800, 600, "ti render");
+
+	scene scene;
+	scene.create_mesh("./mesh/StarSparrow.mesh");
+
+	render::set_scene(&scene);
+
+	render::run();
 }
