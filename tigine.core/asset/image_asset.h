be5af368ddbc2d617f521c9f90b4179b5e3ccc5d
Jiang Dunchun <jiangdunchun@outlook.com>
2022-06-29 15:23:03 +0800

    add tests

--- tigine.core/asset/image_asset.h
+++ tigine.core/asset/image_asset.h
@@ -1,34 +1,47 @@
 #ifndef __IMAGE_ASSET_H__
 #define __IMAGE_ASSET_H__
 
+#include <glm/glm.hpp>
+
 namespace tigine {
-    class image_file {
+	enum class image_format {
+		R8B,
+		RGB8B,
+		RGBA8B,
+		R16F,
+		RGB16F,
+		RGBA16F
+	};
+
+    class image_asset {
 	private:
 		unsigned int m_component;
 		unsigned int m_width = 0;
 		unsigned int m_height = 0;
-		color_format m_format;
+		image_format m_format;
 		void* m_data = nullptr;
 
 	public:
-		image_file(const std::string& path, color_format format, bool flip_y = true);
-		image_file(unsigned int width, unsigned int height, color_format format);
+		image_asset(const std::string& path, image_format format, bool flip_y = true);
+		image_asset(unsigned int width, unsigned int height, image_format format);
+    	image_asset(const image_asset& img) = delete;
+		image_asset& operator=(const image_asset& img) = delete;
 		~image_file();
 
-		static unsigned int get_component(color_format format);
+		static unsigned int get_component(image_format format);
 		int get_data_index(unsigned int x, unsigned int y) const;
 		void save(const std::string& path) const;
-		const unsigned int& get_width(void) const {
+		unsigned int get_width(void) const {
 			return m_width;
 		}
-		const unsigned int& get_height(void) const {
+		unsigned int get_height(void) const {
 			return m_height;
 		}
-		const color_format& get_color_format(void) {
+		image_format get_format(void) {
 			return m_format;
 		}
-		void set_data(const void* data);
-		const void* get_data(void) {
+		void set_data(void* const data);
+		void* const get_data(void) {
 			return m_data;
 		}
 		glm::vec4 sample_liner(glm::vec2 uv) const;
