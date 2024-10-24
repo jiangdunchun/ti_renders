0d3fa4a52c8f7f12548ee624b2f403a5481e0909
JiangDunchun <jiangdunchun@outlook.com>
2024-10-24 20:01:19 +0800

    fix bug of meta

--- toolkit/meta/parser/meta_parser.cmake
+++
@@ -1,8 +0,0 @@
-function(meta_pre_compile HEAD_FILES AUTO_GENERATED_H_FILE AUTO_GENERATED_CPP_FILE)
-    set(META_PARSER ${META_PARSER_DIR}/meta_parser.py)
-    message(STATUS "****meta start to precompile**** python ${META_PARSER} -f ${HEAD_FILES} -gh ${AUTO_GENERATED_H_FILE} -gc ${AUTO_GENERATED_CPP_FILE} -i ${META_INCLUDE_DIR}")
-    execute_process(COMMAND
-        python ${META_PARSER} -f ${HEAD_FILES} -gh ${AUTO_GENERATED_H_FILE} -gc ${AUTO_GENERATED_CPP_FILE} -i ${META_INCLUDE_DIR}
-    )
-    
-endfunction()
